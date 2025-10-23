a0233a167f8578e8cd09449c8bd80baa7dcc17b1
Greenie115 <dggreen545@gmail.com>
2025-10-23 11:04:18 +0200

    adding routing group for authentication with nested routers for sign in and sign up pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1 className='text-3xl'>Stored - The Only Storage Solution You Need!</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">Stored - The Only Storage Solution You Need!</h1>
+    </div>
   );
 }
